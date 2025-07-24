51909994915346ef1674e6a4d7a43c46394ba84d
victor1206 <victor_rivas89@hotmail.com>
2025-07-23 19:09:44 -0600

    First Commit

---
+++ src/main/java/org/esfe/appsysgroup/AppSysGroupApplication.java
@@ -0,0 +1,13 @@
+package org.esfe.appsysgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSysGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSysGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfe/appsysgroup/AppSysGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.appsysgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSysGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
